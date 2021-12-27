87c73b6f8f3ec4080d23f1e37bc1b285380e5f4d
Gugulothu Rajender Naik <rajjrajender2112@gmail.com>
2021-12-27 13:27:29 +0530

    my first commit

---
+++ list.py
@@ -0,0 +1,99 @@
+'''A list in Python is used to store 
+the sequence of various types of data. 
+Python lists are mutable type its mean 
+we can modify its element after it created. 
+Python consists of six data-types 
+that are capable to store the sequences,
+A list can be defined as a collection of values or items
+of different types.
+The items in the list are separated with the comma (,) 
+and enclosed with the square brackets [].'''
+
+# teja=[1,2.222222,'srikanth',True]#empty list
+# print(teja[2])
+
+# hari=[3,4,5,6,7,7,'fine']
+# hari[0]='lekha'
+# print(hari)
+hari=[3,4,5,6,7,7,'fine',3]
+'''
+start stop jump (or step)
+# '''
+# print(hari[0:5])
+# print(hari[0:5:3])
+
+
+# hari=[3,4,5,6,7,7,'fine',3]
+# print(hari[5:0:-1])
+'''
+task 9
+list slicing postive negative 
+'''
+
+'''
+list functions
+
+append
+clear
+copy
+count
+extend
+index
+insert
+pop
+remove
+reverse
+sort
+'''
+
+hari=[3,4,5,6,7,7,'fine',3]
+#hari.append(['srikanth'])
+#hari.extend([1,2,3,4,5,6])
+# hari.clear()
+# s=hari.count(3)
+# vishwa=hari.copy()
+# hari.append(2222)
+# print(hari.index(3))
+'''
+task 10
+index one element   output 0,7  for if list print
+remove more than one element of same  
+'''
+
+hari=[3,4,5,6,7,7,3]
+# hari.insert(3,'kiran')
+# hari.pop(0)
+# #remove
+# hari.remove(3)
+# hari.reverse()
+# hari.sort(reverse=True)
+# print(hari)
+
+# l=[1,2,3,4,5,[1,2,3,4]]
+# print(l[5][3])
+
+
+# l=[1,2,3,4,6,'hemanth']
+# for kumari in l:
+#     print(kumari)
+  
+
+# harish=[kumari for kumari in l]
+# print(harish)
+
+
+
+
+
+
+'''
+Characteristics of Lists
+The element of the list can access by index.
+The lists are the mutable type.
+A list can store the number of various elements.
+'''
+
+l=[1,2,34,4]
+l.append(3)
+print(l)
+l.extend()
---
+++ tuple.py
@@ -0,0 +1,81 @@
+'''Tuple is used to store the sequence of immutable.
+The tuple is similar to lists since the value 
+of the items stored in the list can be changed, 
+whereas the tuple is immutable,
+and the value of the items stored in the tuple cannot be changed.
+A tuple can be written as the collection of comma-separated (,) 
+values enclosed with the small () brackets.'''
+t=(1,3,5,6) #crud not possible
+# print(t[0])
+
+# for i in t:
+#     print(i)
+
+print(max(t))
+print(min(t))
+print(sum(t))
+print(len(t))
+
+
+# t=(1,3,5,6)
+# t1=(2,3,4,5)
+# print(t+t1)
+# t=(1,3,5,6)
+
+# print(1 in t)
+
+'''
+task 11
+number guessing
+
+list for elif print input if else break range varible datatype
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+1	The literal syntax of list is shown by the [].
+	The literal syntax of the tuple is shown by the ().
+2	The List is mutable.	
+    The tuple is immutable.
+3	The List has the a variable length.
+	The tuple has the fixed length.
+4	The list provides more functionality than a tuple.
+	The tuple provides less functionality than the list.
+5	The lists are less memory efficient than a tuple.
+	The tuples are more memory efficient because of its immutability.
+
+
+
+'''
